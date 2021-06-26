704d9a27080c08a7e8c942002bcceeb8dafb1df3
Gabriel Locia <locciaa1@gmail.com>
2021-06-26 00:13:47 -0500

    Agregando eventos a la lista de botones de la vista Jobs For You

--- js/JobList.js
+++ js/JobList.js
@@ -1,66 +1,29 @@
 /*** Link JobList*** */
 ((d) => {
   const $linkEdit = d.getElementById('linkYourProfile'),
-    $yourProfile = d.getElementById('yourProfile'),
-    $jobsForYou = d.getElementById('job-for-you');
+    $windowYourProfile = d.getElementById('yourProfile'),
+    $windowJobsForYou = d.getElementById('job-for-you');
 
   $linkEdit.addEventListener('mousedown', (event) => {
-    
-    $yourProfile.classList.add('active', 'show');
+    $windowYourProfile.classList.add('active', 'show', 'edit-absolut');
+    $windowYourProfile.setAttribute('aria-selected', 'true');
 
-    $jobsForYou.classList.remove('show');
-    $jobsForYou.setAttribute('aria-selected', 'false');
-    $yourProfile.setAttribute('aria-selected', 'true');
+    $windowJobsForYou.classList.remove('show', 'active');
+    $windowJobsForYou.setAttribute('aria-selected', 'false');
   });
 })(document);
-
-/******Job Buttons ****** */
-((d) => {
-  const $jobButton = d.getElementById('jobButtonResult'),
-    $windosJobForYou = d.getElementById('job-for-you'),
-    $jobModal = d.getElementById('modal-job-details'),
-    $buttonJobForYou = d.getElementById('jobsForYou-button'),
-    $buttonYourApplication = d.getElementById('yourApplications-button'),
-    $buttonYourProfile = d.getElementById('yourProfile-button'),
-    $buttonModalReturn = d.getElementById('modal-button-return');
-
-  $jobButton.addEventListener('click', (event) => {
-    $jobModal.classList.toggle('none');
-    $windosJobForYou.classList.add('none');
-  });
-
-  $buttonYourApplication.addEventListener('click', (event) => {
-    if (!$buttonJobForYou.classList.contains('none'))
-      $jobModal.classList.add('none');
-  });
-
-  $buttonModalReturn.addEventListener('click', (event) => {
-    $windosJobForYou.classList.toggle('none');
-    if (!$buttonJobForYou.classList.contains('none')) {
-      $jobModal.classList.add('none');
-    }
-  });
-
-  $buttonYourProfile.addEventListener('click', (event) => {
-    if (!$buttonJobForYou.classList.contains('none'))
-      $jobModal.classList.add('none');
-  });
-})(document);
-/******Job Buttons ****** */
 
 /*****Busqueda ************/
 ((d) => {
   const $inputSearch = d.getElementById('jobSearch'),
     $container = d.getElementsByClassName('button-selection');
- 
-  $inputSearch.addEventListener('keyup', (event) => {
 
+  $inputSearch.addEventListener('keyup', (event) => {
     Array.prototype.forEach.call($container, (p, i) => {
-      
-      //obteniendo nombre del trabajo 
+      //obteniendo nombre del trabajo
       aux = p.getAttribute('data-search');
-     
-      aux.toLocaleLowerCase().includes(event.target.value)
+
+      aux.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase())
         ? p.classList.remove('filter')
         : p.classList.add('filter');
     });
@@ -79,50 +42,58 @@ let random = function (min, max) {
 /*****Agregar elementos a al lista jobs for you************/
 
 ((d) => {
-  const $listJobResult = d.getElementById('jobListResult')
+  const $listJobResult = d.getElementById('jobListResult'),
+    $windowYourProfile = d.getElementById('yourProfile'),
+    $windowJobsForYou = d.getElementById('job-for-you'),
+    $jobModal = d.getElementById('modal-job-details');
 
-
-
- //consulta a al banckend en esta caso consulta a un archivo de pruebas
-  fetch('../Jobs.json') 
+  //consulta a al banckend en esta caso consulta a un archivo de pruebas
+  fetch('../Jobs.json')
     .then((resp) => resp.json())
     .then((jobs) => {
-      console.log(jobs);
+      // console.log(jobs);
       for (jobs of jobs) {
-        console.log(jobs);
+        // console.log(jobs);
 
         let ran = random(1, 50);
         let id = 'jobButton' + ran;
-        
-        const $button = document.createElement('button'),
-        $divJobCompany = document.createElement('div'),
-        $imgLogoCompany = document.createElement('img'),
-        $spansearchTitle = document.createElement('span'),
-        $pJobTitle = document.createElement('p'),
-        $spanCompanyAndCity = document.createElement('span'),
-        $pCompanyAndCity = document.createElement('p'),
-        $imgjobNationFlag = document.createElement('img'),
-        $divJobGrup2 = document.createElement('div'),
-        $divhov = document.createElement('div'),
-        $imgVector$ = document.createElement('img'),
-        $divpay = document.createElement('div'),
-        $psalaryRange = document.createElement('p'),
-        $divarow = document.createElement('div'),
-        $pJobTime = document.createElement('p'),
-        $pJobSeniority = document.createElement('p'),
-        $pData = document.createElement('p');
 
-        //Cargar Empleos a la lista ul 
-       
+        const $button = document.createElement('button'),
+          $divJobCompany = document.createElement('div'),
+          $imgLogoCompany = document.createElement('img'),
+          $spansearchTitle = document.createElement('span'),
+          $pJobTitle = document.createElement('p'),
+          $spanCompanyAndCity = document.createElement('span'),
+          $pCompanyAndCity = document.createElement('p'),
+          $imgjobNationFlag = document.createElement('img'),
+          $divJobGrup2 = document.createElement('div'),
+          $divhov = document.createElement('div'),
+          $imgVector$ = document.createElement('img'),
+          $divpay = document.createElement('div'),
+          $psalaryRange = document.createElement('p'),
+          $divarow = document.createElement('div'),
+          $pJobTime = document.createElement('p'),
+          $pJobSeniority = document.createElement('p'),
+          $pData = document.createElement('p');
+
+        //Cargar Empleos a la lista ul
+
         $button.setAttribute('id', id);
         $button.setAttribute('type', 'button');
         $button.setAttribute(
           'class',
-          'list-group-item list-group-item-action button-selection filter'
+          'list-group-item list-group-item-action button-selection filter jobButtonResult'
         );
 
         $button.setAttribute('aria-current', id);
         $button.setAttribute('data-search', jobs.title);
+
+        $button.addEventListener('click', (event) => {
+          $windowJobsForYou.classList.add('none');
+          $jobModal.classList.toggle('none');
+
+          
+        });
 
         $divJobCompany.setAttribute('class', 'job-company d-flex');
         $imgLogoCompany.setAttribute('class', 'log-company');
@@ -197,6 +168,40 @@ let random = function (min, max) {
         /******Extructura html**** */
       }
     });
-
-  
 })(document);
+/*****Agregar elementos a al lista jobs for you************/
+
+/******Job Buttons ****** */
+
+((d) => {
+  const $jobButton = d.getElementById('jobButtonResult'),
+    $windowJobForYou = d.getElementById('job-for-you'),
+    $jobModal = d.getElementById('modal-job-details'),
+    $buttonJobForYou = d.getElementById('jobsForYou-button'),
+    $buttonYourApplication = d.getElementById('yourApplications-button'),
+    $buttonYourProfile = d.getElementById('yourProfile-button'),
+    $buttonModalReturn = d.getElementById('modal-button-return');
+
+  $jobButton.addEventListener("click",event=>{
+    console.log(event)
+    $windowJobForYou.classList.add("none");
+    $jobModal.classList.remove("none");
+  })
+  $buttonYourApplication.addEventListener('click', (event) => {
+    if (!$buttonJobForYou.classList.contains('none'))
+      $jobModal.classList.add('none');
+  });
+
+  $buttonModalReturn.addEventListener('click', (event) => {
+    $windowJobForYou.classList.toggle('none');
+    if (!$buttonJobForYou.classList.contains('none')) {
+      $jobModal.classList.add('none');
+    }
+  });
+
+  $buttonYourProfile.addEventListener('click', (event) => {
+    if (!$buttonJobForYou.classList.contains('none'))
+      $jobModal.classList.add('none');
+  });
+})(document);
+/******Job Buttons ****** */
