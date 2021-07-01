b119b3f066fa627290273d6fc3b28603de334f75
Gabriel Locia <locciaa1@gmail.com>
2021-07-01 01:35:14 -0500

    restructurando código para agregar elementos al modal Jobs Details

--- js/JobList.js
+++ js/JobList.js
@@ -40,153 +40,266 @@ let random = function (min, max) {
 };
 /**********Funcion id aleatorios */
 
+/**********Funcion para configurar atributos de los elementos html */
+const configAtribute = (
+  $button,
+  $divJobCompany,
+  $imgLogoCompany,
+  $spansearchTitle,
+  $pJobTitle,
+  $spanCompanyAndCity,
+  $imgjobNationFlag,
+  $divJobGrup2,
+  $divhov,
+  $imgVector$,
+  $divpay,
+  $psalaryRange,
+  $divarow,
+  $pJobTime,
+  $pJobSeniority,
+  $pData,
+  id,
+  $pCompanyAndCity
+) => {
+  $button.setAttribute('id', id);
+  $button.setAttribute('type', 'button');
+  $button.setAttribute(
+    'class',
+    'list-group-item list-group-item-action button-selection filter jobButtonResult'
+  );
+
+  $button.setAttribute('aria-current', id);
+  $button.setAttribute('data-search', job.title);
+
+  $divJobCompany.setAttribute('class', 'job-company d-flex');
+  $imgLogoCompany.setAttribute('class', 'log-company');
+
+  $imgLogoCompany.setAttribute('class', 'logo-company');
+  id = 'joblogoCompany' + random(1, 100);
+  $imgLogoCompany.setAttribute('id', id);
+  $imgLogoCompany.setAttribute('src', '../assets/Able.png');
+  $imgLogoCompany.setAttribute('alt', 'logoCompany');
+
+  $spansearchTitle.setAttribute('id', 'searchTitle');
+  $spansearchTitle.setAttribute('class', 'd-flex flex-column searchTitle');
+
+  $pJobTitle.setAttribute('class', 'job-title');
+  $pJobTitle.setAttribute('id', 'job-title w-auto');
+  $pJobTitle.innerText = job.title;
+  $spanCompanyAndCity.setAttribute('class', 'd-flex');
+  $pCompanyAndCity.setAttribute('id', 'companyAndCity');
+  $pCompanyAndCity.innerText = job.company + '-' + job.location;
+
+  $imgjobNationFlag.setAttribute('i', 'log-company');
+  id = 'jobNationFlag' + random(1, 100);
+  $imgjobNationFlag.setAttribute('id', id);
+  $imgjobNationFlag.setAttribute('src', '../assets/image 2.png');
+  $imgjobNationFlag.setAttribute('alt', 'flag');
+
+  $divJobGrup2.setAttribute('class', 'job-grup2 d-flex align-items-center');
+  $divhov.setAttribute('class', 'hov');
+  $imgVector$.setAttribute('src', '../assets/Vector$.svg');
+  $imgVector$.setAttribute('alt', '$');
+  $divpay.setAttribute('class', 'pay');
+
+  $psalaryRange.setAttribute('style', 'magin-bottoM:0');
+  $psalaryRange.innerText = job.salary;
+  d = 'M0.5 12L0.500002 0L6.5 6L0.5 12Z';
+  fill = 'white';
+  $divarow.setAttribute('class', 'arow');
+  $pJobTime.setAttribute('class', 'job-time c');
+  $pJobTime.innerText = job.type;
+  $pJobSeniority.setAttribute('class', 'job-grade c');
+  $pJobSeniority.innerText = job.seniority;
+  $pData.setAttribute('class', 'date ');
+  $pData.innerText = 'Today';
+};
+/**********Funcion para configurar atributos de los elementos html */
+
+/**********Funcion para crear estructura html */
+const htmlStructure = (
+  $button,
+  $divJobCompany,
+  $imgLogoCompany,
+  $spansearchTitle,
+  $pJobTitle,
+  $spanCompanyAndCity,
+  $imgjobNationFlag,
+  $divJobGrup2,
+  $divhov,
+  $imgVector$,
+  $divpay,
+  $psalaryRange,
+  $divarow,
+  $pJobTime,
+  $pJobSeniority,
+  $pData,
+  id,
+  $pCompanyAndCity,
+  $listJobResult
+) => {
+  $button.appendChild($divJobCompany);
+  $divJobCompany.appendChild($imgLogoCompany);
+  $divJobCompany.appendChild($spansearchTitle);
+  $spansearchTitle.appendChild($pJobTitle);
+  $spansearchTitle.appendChild($spanCompanyAndCity);
+  $spanCompanyAndCity.appendChild($pCompanyAndCity);
+  $spanCompanyAndCity.appendChild($imgjobNationFlag);
+
+  $button.appendChild($divJobGrup2);
+  $divJobGrup2.appendChild($divhov);
+  $divhov.appendChild($imgVector$);
+  $divhov.appendChild($divpay);
+  $divpay.appendChild($psalaryRange);
+
+  $divJobGrup2.appendChild($pJobTime);
+  $divJobGrup2.appendChild($pJobSeniority);
+  $divJobGrup2.appendChild($pData);
+
+  $listJobResult.appendChild($button);
+};
+/**********Funcion para crear estructura html */
+
+/**********Funcion Creadora de Botones  */
+const createButtonResult = (
+  $listJobResult,
+  $windowJobsForYou,
+  $jobModal,
+  $elementsGrup1,
+  id,
+  job,
+  jobs
+) => {
+  const $button = document.createElement('button'),
+    $divJobCompany = document.createElement('div'),
+    $imgLogoCompany = document.createElement('img'),
+    $spansearchTitle = document.createElement('span'),
+    $pJobTitle = document.createElement('p'),
+    $spanCompanyAndCity = document.createElement('span'),
+    $pCompanyAndCity = document.createElement('p'),
+    $imgjobNationFlag = document.createElement('img'),
+    $divJobGrup2 = document.createElement('div'),
+    $divhov = document.createElement('div'),
+    $imgVector$ = document.createElement('img'),
+    $divpay = document.createElement('div'),
+    $psalaryRange = document.createElement('p'),
+    $divarow = document.createElement('div'),
+    $pJobTime = document.createElement('p'),
+    $pJobSeniority = document.createElement('p'),
+    $pData = document.createElement('p');
+
+  //Configurando atributos
+  configAtribute(
+    $button,
+    $divJobCompany,
+    $imgLogoCompany,
+    $spansearchTitle,
+    $pJobTitle,
+    $spanCompanyAndCity,
+    $imgjobNationFlag,
+    $divJobGrup2,
+    $divhov,
+    $imgVector$,
+    $divpay,
+    $psalaryRange,
+    $divarow,
+    $pJobTime,
+    $pJobSeniority,
+    $pData,
+    id,
+    $pCompanyAndCity
+    );
+  //Configurando atributos
+    
+    //Agregando eventos a cada boton de la lista
+    $button.addEventListener('click', (event) => {
+      $windowJobsForYou.classList.add('none');
+      $jobModal.classList.toggle('none');
+      console.log(event);
+      console.log($button.dataset.search);
+      const data = jobs.find((data) => data.title == $button.dataset.search);
+      console.log($elementsGrup1);
+      $elementsGrup1[1].innerText = data.title;
+      $elementsGrup1[2].innerText = data.type;
+      $elementsGrup1[3].innerText = data.seniority;
+      $elementsGrup1[4].innerText = data.jobIntroducction;
+      const $delateLi = $elementsGrup1[5].querySelectorAll('li');
+
+      if($delateLi){
+        $delateLi.forEach(element=>{
+          $elementsGrup1[5].removeChild(element);
+        });
+      }
+
+      console.log("li ha eliminar:", $delateLi);
+      data.expected.forEach(element =>{
+      
+        const li = document.createElement("li");
+        li.innerText = element;
+        console.log(li);
+        $elementsGrup1[5].appendChild(li);
+      });
+      // $elementsGrup1[5].innerText = data.jobIntroducction;
+      
+    });
+    //Agregando eventos a cada boton de la lista
+    
+  /******Extructura html**** */
+  htmlStructure(
+    $button,
+    $divJobCompany,
+    $imgLogoCompany,
+    $spansearchTitle,
+    $pJobTitle,
+    $spanCompanyAndCity,
+    $imgjobNationFlag,
+    $divJobGrup2,
+    $divhov,
+    $imgVector$,
+    $divpay,
+    $psalaryRange,
+    $divarow,
+    $pJobTime,
+    $pJobSeniority,
+    $pData,
+    id,
+    $pCompanyAndCity,
+    $listJobResult
+    );
+  /******Extructura html**** */
+};
+
 /*****Agregar elementos a al lista jobs for you************/
 ((d) => {
   const $listJobResult = d.getElementById('jobListResult'),
     //$windowYourProfile = d.getElementById('yourProfile'),
     $windowJobsForYou = d.getElementById('job-for-you'),
     $jobModal = d.getElementById('modal-job-details'),
-    $elementsGrup1 = document.getElementsByClassName("dat-modalElement");
-    
+    $elementsGrup1 = document.getElementsByClassName('dat-modalElement');
+
   //consulta al banckend en esta caso consulta a un archivo de pruebas
   fetch('../Jobs.json')
     .then((resp) => resp.json())
     .then((jobs) => {
-      console.log("Jobss",jobs);
+      console.log('Jobss', jobs);
       for (job of jobs) {
-         console.log(job);
+        console.log(job);
 
         let ran = random(1, 50);
         let id = 'jobButton' + ran;
-
-        const $button = document.createElement('button'),
-          $divJobCompany = document.createElement('div'),
-          $imgLogoCompany = document.createElement('img'),
-          $spansearchTitle = document.createElement('span'),
-          $pJobTitle = document.createElement('p'),
-          $spanCompanyAndCity = document.createElement('span'),
-          $pCompanyAndCity = document.createElement('p'),
-          $imgjobNationFlag = document.createElement('img'),
-          $divJobGrup2 = document.createElement('div'),
-          $divhov = document.createElement('div'),
-          $imgVector$ = document.createElement('img'),
-          $divpay = document.createElement('div'),
-          $psalaryRange = document.createElement('p'),
-          $divarow = document.createElement('div'),
-          $pJobTime = document.createElement('p'),
-          $pJobSeniority = document.createElement('p'),
-          $pData = document.createElement('p');
-
-        //Cargar Empleos a la lista ul
-
-        $button.setAttribute('id', id);
-        $button.setAttribute('type', 'button');
-        $button.setAttribute(
-          'class',
-          'list-group-item list-group-item-action button-selection filter jobButtonResult'
+        createButtonResult(
+          $listJobResult,
+          $windowJobsForYou,
+          $jobModal,
+          $elementsGrup1,
+          id,
+          job,
+          jobs
         );
-
-        $button.setAttribute('aria-current', id);
-        $button.setAttribute('data-search', job.title);
-        
-        //Agregando eventos a cada boton de la lista
-        // $button.addEventListener("click",event=>{
-        //   console.log(event)
-        //   $windowJobForYou.classList.add("none");
-        //   $jobModal.classList.remove("none");
-        // })
-
-        $button.addEventListener('click', (event) => {
-         
-          $windowJobsForYou.classList.add('none');
-          $jobModal.classList.toggle('none');
-          console.log(event)
-           console.log($button.dataset.search)
-          const data = jobs.find(data => data.title == $button.dataset.search)
-          console.log(data);
-          $elementsGrup1[1].innerText = data.title;
-          $elementsGrup1[2].innerText = data.type;
-          $elementsGrup1[3].innerText = data.seniority;
-          $elementsGrup1[4].innerText = data.jobIntroducction;
-
-        });
-
-        $divJobCompany.setAttribute('class', 'job-company d-flex');
-        $imgLogoCompany.setAttribute('class', 'log-company');
-
-        $imgLogoCompany.setAttribute('class', 'logo-company');
-        id = 'joblogoCompany' + random(1, 100);
-        $imgLogoCompany.setAttribute('id', id);
-        $imgLogoCompany.setAttribute('src', '../assets/Able.png');
-        $imgLogoCompany.setAttribute('alt', 'logoCompany');
-
-        $spansearchTitle.setAttribute('id', 'searchTitle');
-        $spansearchTitle.setAttribute(
-          'class',
-          'd-flex flex-column searchTitle'
-        );
-
-        $pJobTitle.setAttribute('class', 'job-title');
-        $pJobTitle.setAttribute('id', 'job-title w-auto');
-        $pJobTitle.innerText = job.title;
-        $spanCompanyAndCity.setAttribute('class', 'd-flex');
-        $pCompanyAndCity.setAttribute('id', 'companyAndCity');
-        $pCompanyAndCity.innerText = job.company + '-' + job.location;
-
-        $imgjobNationFlag.setAttribute('i', 'log-company');
-        id = 'jobNationFlag' + random(1, 100);
-        $imgjobNationFlag.setAttribute('id', id);
-        $imgjobNationFlag.setAttribute('src', '../assets/image 2.png');
-        $imgjobNationFlag.setAttribute('alt', 'flag');
-
-        $divJobGrup2.setAttribute(
-          'class',
-          'job-grup2 d-flex align-items-center'
-        );
-        $divhov.setAttribute('class', 'hov');
-        $imgVector$.setAttribute('src', '../assets/Vector$.svg');
-        $imgVector$.setAttribute('alt', '$');
-        $divpay.setAttribute('class', 'pay');
-
-        $psalaryRange.setAttribute('style', 'magin-bottoM:0');
-        $psalaryRange.innerText = job.salary;
-        d = 'M0.5 12L0.500002 0L6.5 6L0.5 12Z';
-        fill = 'white';
-        $divarow.setAttribute('class', 'arow');
-        $pJobTime.setAttribute('class', 'job-time c');
-        $pJobTime.innerText = job.type;
-        $pJobSeniority.setAttribute('class', 'job-grade c');
-        $pJobSeniority.innerText = job.seniority;
-        $pData.setAttribute('class', 'date ');
-        $pData.innerText = 'Today';
-
-        /******Extructura html**** */
-
-        $button.appendChild($divJobCompany);
-        $divJobCompany.appendChild($imgLogoCompany);
-        $divJobCompany.appendChild($spansearchTitle);
-        $spansearchTitle.appendChild($pJobTitle);
-        $spansearchTitle.appendChild($spanCompanyAndCity);
-        $spanCompanyAndCity.appendChild($pCompanyAndCity);
-        $spanCompanyAndCity.appendChild($imgjobNationFlag);
-
-        $button.appendChild($divJobGrup2);
-        $divJobGrup2.appendChild($divhov);
-        $divhov.appendChild($imgVector$);
-        $divhov.appendChild($divpay);
-        $divpay.appendChild($psalaryRange);
-
-        $divJobGrup2.appendChild($pJobTime);
-        $divJobGrup2.appendChild($pJobSeniority);
-        $divJobGrup2.appendChild($pData);
-
-        $listJobResult.appendChild($button);
-        /******Extructura html**** */
       }
     });
 })(document);
 /*****Agregar elementos a al lista jobs for you************/
-
 
 /******Job ButtonsResult ****** */
 ((d) => {
@@ -198,7 +311,6 @@ let random = function (min, max) {
     $buttonYourProfile = d.getElementById('yourProfile-button'),
     $buttonModalReturn = d.getElementById('modal-button-return');
 
- 
   $buttonYourApplication.addEventListener('click', (event) => {
     if (!$buttonJobForYou.classList.contains('none'))
       $jobModal.classList.add('none');
